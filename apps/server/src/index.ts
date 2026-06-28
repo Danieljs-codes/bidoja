eeb18abd7e2398c6801786def783228f1af4505b
Olamide Onabiyi <onabiyiolamide@gmail.com>
2026-06-28 19:52:32 +0100

    Initialized the api contract server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,5 @@
 import { Console, Effect } from "effect";
 
-const program = Console.log("Hello from server");
+const program = Console.log("Hello from server. Crazy part is that this shit is running effect.");
 
 await Effect.runPromise(program);
--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       neverBundle: [
         // Carve out native addons, They can't be bundled (This should always contain native modules that are used in the server package e.g argon2, bcrypt, etc.)
       ],
+      onlyBundle: false,
     },
   },
 });
